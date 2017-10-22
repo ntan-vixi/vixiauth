f5e6d7e2e84207e3541502d98c083d893d6b1be2
ntan-vixi <33009981+ntan-vixi@users.noreply.github.com>
2017-10-23 01:59:58 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: 1337,
+    API_KEY: 'vzPBrO2yRewE9pKyHd4mhnDB45P6OG74',
+    SECRET: "SUPERSECRETSECRET"
+};
---
+++ server/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    PORT: 1337,
+    API_KEY: 'LfQVFBWgiDImhnhaYPjShbhUiNQhJ0rK',
+    SECRET: "SUPERSECRETSECRET"
+};
